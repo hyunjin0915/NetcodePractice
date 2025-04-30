af8fadd988701d48f2cd0ab728e43d5c2a15bd3b
Toothless <hyunjinney@naver.com>
2025-04-30 17:44:24 +0900

    Initial commit

---
+++ Assets/ClientPlayerMove.cs
@@ -0,0 +1,31 @@
+using Unity.Netcode;
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClientPlayerMove : NetworkBehaviour
+{
+    [SerializeField] private PlayerInput m_PlayerInput;
+    [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
+    [SerializeField] private ThirdPersonController m_ThirdPersonController;
+
+    private void Awake()
+    {
+        m_StarterAssetsInputs.enabled = false;
+        m_PlayerInput.enabled = false;
+        m_ThirdPersonController.enabled = false;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        if (IsOwner)
+        {
+            m_StarterAssetsInputs.enabled = true;
+            m_PlayerInput.enabled = true;
+            m_ThirdPersonController.enabled = true;
+        }
+    }
+
+}
